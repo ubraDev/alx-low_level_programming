432236ad41753ba9695f77426e4383b62759886b
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-03-16 09:08:55 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>                                                                  
+/*                                                                                 
+ *  Description: main function - Print a string using  stdout.                            
+ *   Return    : 0 if success.                                                        
+*/                                                                                  
+int main(void)                                                                      
+{                                                                                   
+printf("rogramming is like building a multilingual puzzle,\n");                
+return (0);
