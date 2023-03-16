56380724775f8c809cb45a7eea7d091ccd3fc46c
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-03-16 09:37:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * Description: main - Prints out a string to stdout
- * Return: 0 if success
- * */
+ * main - this is main function
+ * Return - ruturn 0
+**/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle);
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
