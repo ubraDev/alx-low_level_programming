c8880436b2bba36a1365e22c6d65f9f43bc7d5c6
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-03-17 10:11:49 -0700

    check if number positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* Discription : main - check if number positive or negative */
-/* Return : return number of eache cas*/
+
+/**
+* Discription : main - check if number positive or negative
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 
@@ -11,11 +14,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* my code */
-	if (n >= 0)
-		printf("this number is positive %d", n);
+	if (n > 0)
+		printf("%d this number is positive \n", n);
 	if (n == 0)
-		printf("this number is zero %d ", n);
+		printf("this number is zero \n", n);
 	if (n < 0)
-		printf("this number is negative %d ", n);
+		printf("this number is negative \n", n);
+
 	return (0);
 }
