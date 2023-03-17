20ad5ab1eb38c04286066aae6e639a51df9aba93
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-03-17 13:10:23 -0700

    fix

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - program that prints the alphabet in lowercase
+*and then in uppercase
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
