2807f8908bdb7a8f4ed8fbc836cb27eb3bcdcae5
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-03-17 13:44:08 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - print all single digit numbers.
+* Return: Always 0 (Success)
+**/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d all numbers \n", i);
+	}
+	putchar('\n');
+	return (0);
+}
