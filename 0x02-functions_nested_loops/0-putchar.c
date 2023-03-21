a960b709923fbe5393d2d1da517eb647e63562b6
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-03-21 15:58:50 -0700

    fix

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* Discriptioni: main - putchar.
+* return - 0 whene sccuess$
+**/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
