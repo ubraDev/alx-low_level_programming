132af442a748867b538dee6f31d417e9f857d4c5
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-03-21 18:12:46 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_islower - function to check for lowercase character.
+*Return: Always 0.
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
