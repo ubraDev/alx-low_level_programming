92f874446a0355aa48fdc995a7735fac3c4c81c6
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-03-21 19:10:02 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *times_table - Prints the 9 times table, starting with 0.
- *
- * Return: Always 0.
- */
+*times_table - Prints the 9 times table, starting with 0.
+*
+* Return: Always 0.
+*/
 void times_table(void)
 {
 	int x;
