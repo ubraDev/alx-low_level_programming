20a4c526cb19d083cb81bbc5aede3e396aba30d9
root <Brahim El Alami Chentoufi>
2023-04-10 15:22:13 -0700

    fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,14 +13,14 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
-	if(argc == 3)
-
+		if (argc == 3)
 		{
 			a = atoi(argv[1]);
 			b = atoi(argv[2]);
 			printf("%d\n", a * b);
 			return (0);
 		}
+
 	printf("Error\n");
 	return (1);
 }
