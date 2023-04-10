b22d0a30eb8683339718fe38dce7a73d8900b1c1
root <Brahim El Alami Chentoufi>
2023-04-10 16:13:47 -0700

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,6 +6,7 @@
 /**
  * check_num - check - string there are digit
  * @str: array str
+ *
  * Return: Always 0 (Success)
  */
 
@@ -25,7 +26,6 @@ int check_num(char *str)
 	}
 	return (1);
 }
-
 /**
  * main - Print the name of the program
  * @argc: Count arguments
@@ -33,38 +33,32 @@ int check_num(char *str)
  *
  * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
-
 {
-
-
 	int count;
+
 	int str_to_int;
+
 	int sum = 0;
+
 
 	count = 1;
 
 	while (count < argc)
-
 	{
-	if (check_num(argv[count]))
-	{
-	str_to_int = atoi(argv[count]); sum +=
-	str_to_int;
-	}
-
-	else
-	{
-	printf("Error\n");
-	return (1);
-
-	}
-
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 		count++;
 	}
-
-	printf("%d\n", sum)
+	printf("%d\n", sum);
 	return (0);
-
 }
+
