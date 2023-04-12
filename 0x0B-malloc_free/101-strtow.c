131ee0f89f89dd8a00c3406f1e2a5b7d6164d0fe
root <Brahim El Alami Chentoufi>
2023-04-11 17:41:19 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include "main.h"
-/*
-*count_word - helper function to count the number of words in a string
-*@s: string to evaluate
-*Return: number of words
-*/
+
+/**
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
+ */
 
 int count_word(char *s)
 {
@@ -30,11 +32,12 @@ int count_word(char *s)
 }
 
 /**
-* **strtow - splits a string into words
-*@str: string to split
-*Return: pointer to an array of strings (Success)
-*or NULL (Error)
-*/
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
 
 char **strtow(char *str)
 {
