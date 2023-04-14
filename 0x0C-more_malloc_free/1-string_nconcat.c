ff04a02d9d941a4ebec79947d06686d9b6c5f24b
root <Brahim El Alami Chentoufi>
2023-04-13 17:25:12 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,9 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	if (s1 == NULL)
-		s1 = "";
+		s1 = 0;
 	if (s2 == NULL)
-		s2 = "";
+		s2 = 0;
 
 	size_t s1_len = strlen(s1);
 	size_t s2_len = strlen(s2);
