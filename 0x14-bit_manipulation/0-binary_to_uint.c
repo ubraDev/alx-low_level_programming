37a1430ae48fa76b8d1ed3aa919244423b7ad66e
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-05-05 18:09:18 +0100

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int res = 0;
-    int i;
+unsigned int res = 0;
+int i;
 
-    if (!b)
-        return (0);
+if (!b)
+return (0);
 
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] < '0' || b[i] > '1')
-            return (0);
-        res = (res << 1) + (b[i] - '0');
-    }
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+res = (res << 1) + (b[i] - '0');
+}
 
-    return (res);
+return (res);
 }
