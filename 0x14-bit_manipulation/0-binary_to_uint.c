0effbb05552b58fd597f0791139e473dbff168a5
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-05-05 18:19:46 +0100

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-* binary_to_uint - converts a binary number to an
+* binary_to_uint : converts a binary number to an
 * unsigned int.
 * @b: binary.
 * Return: unsigned int.
