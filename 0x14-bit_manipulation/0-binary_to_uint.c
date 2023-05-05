535cafd2a06053959842a724e52220e4322e6777
Brahim EL Alami Chentoufi <bra.el.alami@gmail.com>
2023-05-05 09:59:55 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+
+/*
+ * description : function that converts a binary number to an unsigned int
+*/
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int res = 0;
+int i;
+
+if (b == NULL)
+return (0);
+
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+res = (res << 1) + (b[i] - '0');
+}
+
+return (res);
+}
