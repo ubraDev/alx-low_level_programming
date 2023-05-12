e662879d71c7a02d330cc78c9c4ca58c0935387c
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-05-12 22:58:59 +0100

    add

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+if (filename == NULL)
+{
+return (0);
+}
+
+int fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+return (0);
+}
+
+char *buffer = (char *)malloc(letters + 1);
+if (buffer == NULL)
+{
+close(fd);
+return (0);
+}
+
+ssize_t bytes_read = read(fd, buffer, letters);
+if (bytes_read == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+
+ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+
+free(buffer);
+close(fd);
+return (bytes_written);
+}
