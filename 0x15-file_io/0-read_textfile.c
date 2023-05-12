31c2f51699f131e19bcce32f3df939824f8931e9
Brahim El Alami Chentoufi <bra.el.alami@gmail.com>
2023-05-12 23:02:30 +0100

    add

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,6 +3,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+
+/**
+*read_textfile- Read text file print to STDOUT.
+*@filename: text file being read
+*@letters: number of letters to be read
+*Return: w- actual number of bytes read and printed
+*0 when function fails or filename is NULL.
+*/
+
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 if (filename == NULL)
