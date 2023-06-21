f95a4d8e408284b06482702de0eee0fb6e5c63bd
BRAHIM EL ALAMI <bra.el.alami@gmail.com>
2023-06-21 16:34:13 -0700

    fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 #include <stdio.h>
 
 /**
@@ -9,6 +10,8 @@
  **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
