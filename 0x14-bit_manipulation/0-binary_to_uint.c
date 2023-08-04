42c6bff7c6075196e049740618eb421d958e9ead
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-04 02:25:37 -0700

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,24 @@
-
 #include "main.h"
 
 /**
 * binary_to_uint - function that converts a binary number to an unsigned int.
 * unsigned int.
-* @b: binary.
+* @n: binary.
 * Return: unsigned int.
 */
-unsigned int binary_to_uint(const char *b)
-{
-unsigned int res = 0;
-int i;
-
-if (!b)
-return (0);
-
-for (i = 0; b[i] != '\0'; i++)
-{
-if (b[i] < '0' || b[i] > '1')
-return (0);
-res = (res << 1) + (b[i] - '0');
+void print_binary(unsigned long int n) {
+if (n > 1) {
+print_binary(n >> 1); // Right shift the number by 1
+}
+putchar((n & 1) + '0'); // Print the least significant bit
 }
 
-return (res);
+int main() {
+unsigned long int num;
+printf("Enter a number: ");
+scanf("%lu", &num);
+printf("Binary representation: ");
+print_binary(num);
+printf("\n");
+return (0);
 }
