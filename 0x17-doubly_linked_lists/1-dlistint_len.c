08149f2a9d56743a8a2459d27bea9e55d3866f2d
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-18 08:06:38 -0700

    fix

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in
+ * a double linked list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t node_count = 0;
+
+while (h != NULL)
+{
+h = h->next;
+node_count++;
+}
+
+return node_count;
+}
