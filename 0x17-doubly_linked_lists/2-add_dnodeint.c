02444050f9a62e97565f8402bb332718658fcb45
BRAHIM EL ALAMI CHENTOUFI <bra.el.alami@gmail.com>
2023-08-18 08:19:51 -0700

    fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 {
-return NULL;
+return (NULL);
 }
 
 new_node->n = n;
